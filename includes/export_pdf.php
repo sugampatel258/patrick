a1c93d7e59a7824df0ab9f56bc6c8e55f0c75dec
Sugam Patel <sugampatel258@gmail.com>
2019-04-12 00:00:22 +0530

    pdf design

--- includes/export_pdf.php
+++ includes/export_pdf.php
@@ -26,11 +26,15 @@ if(isset($_REQUEST['products'])) {
 	// }
 	// $content .= "</table>";
 
-	$content = '<html xmlns="http://www.w3.org/1999/xhtml">
-            <head>
-            <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700" rel="stylesheet">
-            <link href="https://fonts.googleapis.com/css?family=Fira+Sans+Extra+Condensed:400,900" rel="stylesheet">
-            </head>
+	$content = '<head>
+                <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0 " />
+                <meta name="format-detection" content="telephone=no" />
+
+                <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700" rel="stylesheet">
+                <link href="https://fonts.googleapis.com/css?family=Fira+Sans+Extra+Condensed:400,900" rel="stylesheet">
+                </head>
             <body style="margin:0px; padding:0px;" bgcolor="#ffffff">
             <table width="100%" border="0" cellspacing="0" cellpadding="0" align="center" bgcolor="#ffffff">
               <tr>
@@ -41,14 +45,13 @@ if(isset($_REQUEST['products'])) {
                         <table width="600" border="0" cellspacing="0" cellpadding="0" align="center">
                             <tr>
                                 <td valign="top" align="center" background="'.MY_PLUGIN_PATH.'assets/images/background.jpg" width="100%" height="400" style="background-repeat:no-repeat; background:url('.MY_PLUGIN_PATH.'assets/images/background.jpg);">
-                                    <table width="500" border="0" cellspacing="0" cellpadding="0" align="center">
+                                        <table width="500" border="0" cellspacing="0" cellpadding="0" align="center">
                                             <tr><td>&nbsp;</td></tr>
                                             <tr><td>&nbsp;</td></tr>
                                             <tr>
-                                                <td style="text-align: center; background: rgba(255,255,255,0.6);">
-                                                    <h2 style="font-size: 64px; line-height: 68px; margin: 20px 0 25px; font-style: italic; font-weight: bold; font-family: "Fira Sans Extra Condensed", sans-serif; letter-spacing: 3px;"></h2>
-                                                    <h4 style="font-size: 54px; line-height: 68px; font-weight: bold; margin: 0 0 25px;">'.$_REQUEST['dis'].'%</h4>
-                                                    <h5 style="text-transform: uppercase; font-size: 24px; letter-spacing: 3px; font-weight: bold;">Discount On</h5>
+                                                <td style="text-align: center; background: rgba(255,255,255,0.6); height: 292px;">
+                                                    <h4 style="font-size: 54px; line-height: 68px; font-weight: bold; margin: 25px 0;">'.$_REQUEST['dis'].'%</h4>
+                                                    <h5 style="text-transform: uppercase; font-size: 24px; letter-spacing: 3px; font-weight: bold; margin: 20px 0;">Discount On</h5>
                                                 </td>
                                             </tr>
                                         </table>
@@ -63,17 +66,17 @@ if(isset($_REQUEST['products'])) {
                     $content .= '<td align="center" valign="top" width="280" style="text-align: center;">
                                     <table width="280" border="0" cellspacing="0" cellpadding="0" align="center">
                                         <tr>
-                                            <td align="center" valign="top">
-                                                <img src="'.get_the_post_thumbnail_url( $post->ID, 'full' ).'" alt="" title="" style="width: 100%; height: auto; max-width: 160px;">
+                                            <td align="center" valign="top" width="280" style="width: 280px;">
+                                                <img class="product-img" src="'.get_the_post_thumbnail_url( $post->ID, 'full' ).'" alt="" title="" min-height="150px" max-width="100px">
                                             </td>
                                         </tr>
                                         <tr>
-                                            <td>
+                                            <td align="center" valign="top" width="280" style="width: 280px;">
                                                 <h5 style=" font-size: 20px; font-weight: bold; margin: 20px 0;">'.$post->post_title.'</h5>
                                             </td>
                                         </tr>
                                         <tr>
-                                            <td>
+                                            <td align="center" valign="top" width="280" style="width: 280px;">
                                                 <p style="font-size: 16px; line-height: 24px; font-weight: bold;">Price:&nbsp;<span>&euro;'.$product->get_regular_price().'</span></p>
                                             </td>
                                         </tr>
@@ -95,8 +98,10 @@ if(isset($_REQUEST['products'])) {
             </html>';
 	$html = "test html";
     if( ! empty( $posts ) ) {
-     	$stylesheet = file_get_contents('../assets/css/htmltopdf.css');
-	    $mpdf->WriteHTML($stylesheet,1);
+        //$stylesheet = file_get_contents('https://fonts.googleapis.com/css?family=PT+Sans:400,700');
+        //$stylesheet2 = file_get_contents('https://fonts.googleapis.com/css?family=Fira+Sans+Extra+Condensed:400,900');
+     	$stylesheet3 = file_get_contents('../assets/css/htmltopdf.css');
+	    $mpdf->WriteHTML($stylesheet3,1);
 	    $mpdf->WriteHTML($content, 2);
 	    
        
